cd0cca97222e8267b69668a29b4ce3ad8c0cfeea
yuxichen11@gmail.com <yuxichen11@gmail.com>
2018-05-16 20:35:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,11 +13,9 @@ function yourAnimal() {
 }
 
 function add2(n) {
-  const two = 2
-  return n + two
-
+  const two = 2;
+  return n + two;
   // Feel free to move things around!
-  
 }
 
 var funkyFunction = function() {
