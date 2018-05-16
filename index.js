6342a85f6f2be06bc82d4c1b22e0988f74d48b8f
yuxichen11@gmail.com <yuxichen11@gmail.com>
2018-05-16 20:36:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function add2(n) {
   // Feel free to move things around!
 }
 
-var funkyFunction = function() {
+var funkyFunction() = function() {
   return function() {
     return "FUNKY!"
   }
